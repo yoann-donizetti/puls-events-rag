cf2c7ccdd4ad8fe36b633a0efa0d13697da27ac6
Yoann <yoann.donizetti@gmail.com>
2026-03-10 08:28:22 +0100

    Création prepare_document.py

---
+++ src/embeddings/prepare_documents.py
@@ -0,0 +1,14 @@
+"""
+Prépare les documents à vectoriser pour FAISS.
+Entrée :
+data/processed/eventes_*.jsonl
+Sortie :
+liste de documents LangChain
+"""
+
+def main ():
+    print("Préparation des documents à vectoriser")
+
+if __name__ == "__main__":
+    main()
+
